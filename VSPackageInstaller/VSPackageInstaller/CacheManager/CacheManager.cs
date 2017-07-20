3b09cdfaa85ffca954efbab3458339804f80dbe6
Christian Gunderman <chgund@microsoft.com>
2017-07-19 23:12:56 -0700

    Empty cache manager.

---
+++ VSPackageInstaller/VSPackageInstaller/CacheManager/CacheManager.cs
@@ -0,0 +1,45 @@
+﻿namespace VSPackageInstaller.CacheManager
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    internal sealed class CacheManager<TItem>
+    {
+        public bool CacheFileExists => throw new NotImplementedException();
+
+        public DateTime? LastUpdate => throw new NotImplementedException();
+
+        public IReadOnlyList<TItem> Snapshot;
+
+        public void Init()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Create(IEnumerable<TItem> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRange(IEnumerable<TItem> items)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task LoadCacheFileAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SaveCacheFileAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task LoadIfCacheFileOlderThan(DateTime cutoffDate)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
