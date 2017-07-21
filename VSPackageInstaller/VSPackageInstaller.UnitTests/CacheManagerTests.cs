b498d3b2db64c9568f71e4100f137a725a41a67f
Christian Gunderman <chgund@microsoft.com>
2017-07-20 23:29:29 -0700

    Basic in-memory cache manager.

--- VSPackageInstaller/VSPackageInstaller.UnitTests/CacheManagerTests.cs
+++ VSPackageInstaller/VSPackageInstaller.UnitTests/CacheManagerTests.cs
@@ -1,14 +1,117 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using VSPackageInstaller.Cache;
+using System.Threading;
 
 namespace VSPackageInstaller.UnitTests
 {
     [TestClass]
     public class CacheManagerTests
     {
+        public string CacheFilePath = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+
         [TestMethod]
-        public void TestMethod1()
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_NullCacheFilePath()
         {
+            new CacheManager<int>(null);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_WhiteSpaceCacheFilePath()
+        {
+            new CacheManager<int>("  ");
+        }
+
+        [TestMethod]
+        public void Constructor_InitiallyEmpty()
+        {
+            var manager = new CacheManager<int>(CacheFilePath);
+
+            Assert.IsFalse(manager.CacheFileExists);
+            Assert.IsNull(manager.LastCacheFileUpdateTimeStamp);
+            Assert.IsTrue(DateTime.UtcNow.Subtract(manager.LastUpdateTimeStamp.Value).TotalMilliseconds < 100);
+            Assert.IsNotNull(manager.Snapshot);
+            Assert.AreEqual(0, manager.Snapshot.Count);
+        }
+
+        [TestMethod]
+        public void CacheFilePath_ResolvesToFullPath()
+        {
+            var manager = new CacheManager<int>("foofile");
+
+            Assert.IsTrue(manager.CacheFilePath.Split(Path.DirectorySeparatorChar).Length > 1);
+        }
+
+        [TestMethod]
+        public void CacheFileExists_LastUpdated_CorrectResults()
+        {
+            var manager = new CacheManager<int>(CacheFilePath);
+
+            Assert.IsFalse(File.Exists(CacheFilePath));
+            Assert.IsFalse(manager.CacheFileExists);
+            Assert.IsNull(manager.LastCacheFileUpdateTimeStamp);
+            Assert.IsTrue(DateTime.UtcNow.Subtract(manager.LastUpdateTimeStamp.Value).TotalMilliseconds < 20);
+
+            Thread.Sleep(1000);
+
+            Assert.IsTrue(DateTime.UtcNow.Subtract(manager.LastUpdateTimeStamp.Value).TotalMilliseconds >= 1000);
+
+            File.WriteAllText(CacheFilePath, string.Empty);
+
+            Assert.IsTrue(File.Exists(CacheFilePath));
+            Assert.IsTrue(manager.CacheFileExists);
+            Assert.IsTrue(DateTime.UtcNow.Subtract(manager.LastCacheFileUpdateTimeStamp.Value).TotalMilliseconds < 20);
+            Assert.IsTrue(DateTime.UtcNow.Subtract(manager.LastUpdateTimeStamp.Value).TotalMilliseconds < 20);
+
+            Thread.Sleep(1000);
+
+            Assert.IsTrue(DateTime.UtcNow.Subtract(manager.LastUpdateTimeStamp.Value).TotalMilliseconds >= 1000);
+
+            File.Delete(CacheFilePath);
+
+            Assert.IsFalse(File.Exists(CacheFilePath));
+            Assert.IsFalse(manager.CacheFileExists);
+            Assert.IsNull(manager.LastCacheFileUpdateTimeStamp);
+            Assert.IsTrue(DateTime.UtcNow.Subtract(manager.LastUpdateTimeStamp.Value).TotalMilliseconds >= 2000);
+
+            manager.AddRange(new[] { 3 });
+
+            Assert.IsTrue(DateTime.UtcNow.Subtract(manager.LastUpdateTimeStamp.Value).TotalMilliseconds < 20);
+        }
+
+        [TestMethod]
+        public void AddRange_Create_CorrectResults()
+        {
+            var manager = new CacheManager<int>(CacheFilePath);
+
+            Assert.AreEqual(0, manager.Snapshot.Count);
+
+            manager.AddRange(new[] { 3, 2, 1 });
+
+            Assert.AreEqual(3, manager.Snapshot.Count);
+            Assert.AreEqual(3, manager.Snapshot[0]);
+            Assert.AreEqual(2, manager.Snapshot[1]);
+            Assert.AreEqual(1, manager.Snapshot[2]);
+
+            manager.AddRange(new[] { 7, 8, 9 });
+
+            Assert.AreEqual(6, manager.Snapshot.Count);
+            Assert.AreEqual(3, manager.Snapshot[0]);
+            Assert.AreEqual(2, manager.Snapshot[1]);
+            Assert.AreEqual(1, manager.Snapshot[2]);
+            Assert.AreEqual(7, manager.Snapshot[3]);
+            Assert.AreEqual(8, manager.Snapshot[4]);
+            Assert.AreEqual(9, manager.Snapshot[5]);
+
+            manager.Create(new[] { 4, 5, 6 });
+
+            Assert.AreEqual(3, manager.Snapshot.Count);
+            Assert.AreEqual(4, manager.Snapshot[0]);
+            Assert.AreEqual(5, manager.Snapshot[1]);
+            Assert.AreEqual(6, manager.Snapshot[2]);
         }
     }
 }
--- VSPackageInstaller/VSPackageInstaller/Cache/CacheManager.cs
+++ VSPackageInstaller/VSPackageInstaller/Cache/CacheManager.cs
@@ -2,29 +2,87 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Collections.Immutable;
+    using System.IO;
+    using System.Linq;
+    using System.Threading;
     using System.Threading.Tasks;
 
     internal sealed class CacheManager<TItem>
     {
-        public bool CacheFileExists => throw new NotImplementedException();
+        // Modified cross-thread.
+        private Tuple<DateTime?, ImmutableList<TItem>> cacheSnapshot;
 
-        public DateTime? LastUpdate => throw new NotImplementedException();
-
-        public IReadOnlyList<TItem> Snapshot;
-
-        public void Init()
+        public CacheManager(string cacheFilePath)
         {
-            throw new NotImplementedException();
+            if (string.IsNullOrWhiteSpace(cacheFilePath))
+            {
+                throw new ArgumentException($"{nameof(cacheFilePath)} must be non-empty and non-null");
+            }
+
+            this.CacheFilePath = Path.GetFullPath(cacheFilePath);
+
+            this.Create(Enumerable.Empty<TItem>());
         }
+
+        public bool CacheFileExists
+        {
+            get
+            {
+                try
+                {
+                    return File.Exists(this.CacheFilePath);
+                }
+                catch
+                {
+                    // Ok, not quite accurate, but what else can we do?
+                    return false;
+                }
+            }
+        }
+
+        public string CacheFilePath { get; }
+
+        public DateTime? LastUpdateTimeStamp
+        {
+            get
+            {
+                var snapshotTimestamp = Volatile.Read(ref cacheSnapshot).Item1;
+                var cacheFileTimestamp = this.LastCacheFileUpdateTimeStamp;
+
+                if (cacheFileTimestamp == null)
+                {
+                    return snapshotTimestamp;
+                }
+                else if (snapshotTimestamp > cacheFileTimestamp)
+                {
+                    return snapshotTimestamp;
+                }
+                else
+                {
+                    return cacheFileTimestamp;
+                }
+            }
+        }
+
+        public DateTime? LastCacheFileUpdateTimeStamp => (CacheFileExists ? File.GetLastWriteTimeUtc(this.CacheFilePath) : (DateTime?)null);
+
+        public IReadOnlyList<TItem> Snapshot => Volatile.Read(ref this.cacheSnapshot).Item2;
 
         public void Create(IEnumerable<TItem> items)
         {
-            throw new NotImplementedException();
+            Volatile.Write(
+                ref this.cacheSnapshot,
+                Tuple.Create<DateTime?, ImmutableList<TItem>>(DateTime.UtcNow, ImmutableList.CreateRange<TItem>(items)));
         }
 
         public void AddRange(IEnumerable<TItem> items)
         {
-            throw new NotImplementedException();
+            var oldItems = Volatile.Read(ref this.cacheSnapshot).Item2;
+
+            Volatile.Write(
+                ref this.cacheSnapshot,
+                Tuple.Create<DateTime?, ImmutableList<TItem>>(DateTime.UtcNow, oldItems.AddRange(items)));
         }
 
         public async Task LoadCacheFileAsync()
