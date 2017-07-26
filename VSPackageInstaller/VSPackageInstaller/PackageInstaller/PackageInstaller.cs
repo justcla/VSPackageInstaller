9bcfd212f00f16e66fd011e51c6c13789a8564b7
Ahmed Metwally <metoly@hotmail.com>
2017-07-25 18:29:10 -0700

    Added Installer wrapper

--- VSPackageInstaller/VSPackageInstaller/PackageInstaller/PackageInstaller.cs
+++ VSPackageInstaller/VSPackageInstaller/PackageInstaller/PackageInstaller.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.Shell;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Tasks = System.Threading.Tasks;
+using WebEssentials;
+using System.Threading;
 
 namespace VSPackageInstaller.PackageInstaller
 {
+    public sealed class InstallerPackage : AsyncPackage
+
+    {
+
+        public const string _packageGuid = "4f2f2873-be87-4716-a4d5-3f3f047942c4";
+
+
+
+        protected override async Tasks.Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+
+        {
+
+            InstallerService.Initialize(this);
+
+            await InstallerService.RunAsync().ConfigureAwait(false);
+
+        }
+
+        public async Tasks.Task InitalizeAsync()
+        {
+            IProgress<ServiceProgressData> _progress = new System.Progress<ServiceProgressData>();
+
+            await InitializeAsync(new CancellationToken(false), _progress);
+        }
+
+    }
     public class PackageInstaller
     {
         public string PackageTitle
@@ -44,6 +74,8 @@ namespace VSPackageInstaller.PackageInstaller
 
         public string InstallPackage()
         {
+            InstallerPackage _pi = new InstallerPackage();
+            // Todo: add code here to call InstallerPackage with dummy params and handle async
             return "";
         }
 
