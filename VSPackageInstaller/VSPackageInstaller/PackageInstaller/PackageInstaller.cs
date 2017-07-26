4d176af4be3c117da2518c81abf7ddb37a7a9cb8
Ahmed Metwally <metoly@hotmail.com>
2017-07-25 17:43:38 -0700

    Adding WebEssentials

---
+++ VSPackageInstaller/VSPackageInstaller/PackageInstaller/PackageInstaller.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VSPackageInstaller.PackageInstaller
+{
+    public class PackageInstaller
+    {
+        public string PackageTitle
+        {
+            get
+            { return PackageTitle; }
+            set
+            { PackageTitle = value; }
+        }
+
+        public string PackageInstallPath
+        {
+            get
+            { return PackageInstallPath; }
+            set
+            { PackageInstallPath = value; }
+        }
+
+        public PackageInstaller()
+        {
+            PackageTitle = "";
+            PackageInstallPath = "";
+        }
+
+        public string InstallPackage(string _packageTitle, string _packageInstallPath)
+        {
+            PackageTitle = _packageTitle;
+            PackageInstallPath = _packageInstallPath;
+            return InstallPackage();
+        }
+
+        public string InstallPackage(string _packageInstallPath)
+        {
+            PackageInstallPath = _packageInstallPath;
+            return InstallPackage();
+        }
+
+        public string InstallPackage()
+        {
+            return "";
+        }
+
+    }
+}
